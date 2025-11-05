53107d317612a9679d109ae98fdf424bd2536041
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-11-05 19:50:35 +0700

    update: model user

--- BookStoresApi/Models/ApplicationUser.cs
+++ BookStoresApi/Models/ApplicationUser.cs
@@ -4,55 +4,28 @@ using Microsoft.AspNetCore.Identity;
 
 namespace BookStoresApi.Models
 {
-    public class ApplicationUser : IdentityUser<int>
+    public class ApplicationUser : IdentityUser
     {
-        [Key]
-        [Column("user_id")]
-        public override int Id { get; set; }
-
-
         [Required]
         [Column("full_name")]
         [MaxLength(255)]
         public string FullName { get; set; } = string.Empty;
 
-
-        [Required]
-        [Column("email")]
-        [MaxLength(255)]
-        public override string Email { get; set; } = string.Empty;
-
-
         [Column("address")]
         [MaxLength(500)]
         public string? Address { get; set; }
 
-
-        [Column("phone_number")]
-        [MaxLength(50)]
-        public override string? PhoneNumber { get; set; }
-
-
         [Column("birth_date")]
         public DateTime? BirthDate { get; set; }
-
 
         [Column("gender")]
         [MaxLength(20)]
         public string? Gender { get; set; }
 
-
-        [Required]
-        [Column("password")]
-        [MaxLength(255)]
-        public string Password { get; set; } = string.Empty;
-
-
         [Required]
         [Column("user_type")]
         [MaxLength(50)]
         public string UserType { get; set; } = string.Empty; // e.g., admin, customer
-
 
         [Column("is_deleted")]
         public bool IsDeleted { get; set; } = false;
